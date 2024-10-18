02f8f4af5e4ec9f1ddefc6dc24905367f88de18a
Matheus <matheusrobertjs@gmail.com>
2024-10-17 22:41:17 -0300

    Estilização da Página e Posicionamento dos Inputs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import LoginScren from './LoginScren';
 function App() {
 	return (
-		<div className="flex items-center justify-center h-screen bg-green-50">
+		<div className="flex items-center justify-center h-screen bg-zinc-100">
 			<LoginScren />
 		</div>
 	);
--- src/LoginScren.jsx
+++ src/LoginScren.jsx
@@ -1,8 +1,37 @@
+import logoLongchat from './img/logo-longchat.svg';
+
 function LoginScren() {
 	return (
 		<>
-			<div></div>
+			<div className="flex gap-12 items-center">
+				<div className="px-16 py-20 bg-green-800 rounded-2xl">
+					<img src={logoLongchat} alt="logo longchat" />
+				</div>
+				<div>
+					<form action="">
+						<p>Usuário</p>
+						<InptCustom tipo="email" />
+						<p>Senha</p>
+						<InptCustom tipo="password" />
+					</form>
+					<button className="bg-green-800 text-white py-3 rounded-md min-w-[315px]">
+						Login
+					</button>
+				</div>
+			</div>
 		</>
+	);
+}
+
+function InptCustom({ tipo }) {
+	let textPlaceholder = tipo == 'email' ? 'seu@email.com' : '********';
+
+	return (
+		<input
+			className="pl-2 py-3 rounded-md min-w-[315px] mb-6 mt-2"
+			placeholder={textPlaceholder}
+			type={tipo}
+		/>
 	);
 }
 
